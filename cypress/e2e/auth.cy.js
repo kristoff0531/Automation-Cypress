8bde4a1213bdbed2e56ed1481a0471b8b6ebf75c
Kunal <kroy66194@gmail.com>
2024-07-07 18:26:14 +0530

    Authorization

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -10,16 +10,16 @@ describe('Authorization on js', () => {
     cy.get("div[class='example'] p").should('have.contain',"Congratulations")
   })
 
-  it.skip('should not authenticate', ()=>{
-    cy.visit('https://the-internet.herokuapp.com/')
-    cy.wait(2000)
-    cy.get("a[href='/basic_auth']").click()
-    cy.window().then(()=>{
-        cy.on('window:confirm',()=> false)
+//   it.skip('should not authenticate', ()=>{
+//     cy.visit('https://the-internet.herokuapp.com/')
+//     cy.wait(2000)
+//     cy.get("a[href='/basic_auth']").click()
+//     cy.window().then(()=>{
+//         cy.on('window:confirm',()=> false)
 
-    })
-    // cy.on('window:confirm',()=> false)
-    // cy.get("div[class='example'] p").should('have.text',"Congratulations! You must have the proper credentials. ")
-    cy.get("div[class='example'] p").should('have.contain',"Not authorized")
-  })
+//     })
+//     // cy.on('window:confirm',()=> false)
+//     // cy.get("div[class='example'] p").should('have.text',"Congratulations! You must have the proper credentials. ")
+//     cy.get("div[class='example'] p").should('have.contain',"Not authorized")
+//   })
 })
