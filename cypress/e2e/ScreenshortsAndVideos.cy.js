4885879b4e7df88c557d8994feeb188813a8b1fb
Kunal <kroy66194@gmail.com>
2024-07-09 22:56:51 +0530

    captured screenshots & videos

--- cypress/e2e/ScreenshortsAndVideos.cy.js
+++ cypress/e2e/ScreenshortsAndVideos.cy.js
@@ -1,16 +1,19 @@
 describe('Capturing Screenshots and handling videos', () => {
-  // it('should capture screenshot', ()=> {
-  //   cy.visit('https://demo-opencart.com/')
-  //   cy.wait(2000)
-  //   cy.screenshot('Homepage')
-  //   cy.wait(2000)
-  //   cy.get("img[title='Your Store']").screenshot('MyStore')
-  //   cy.wait(2000)
-  //   cy.get("img[alt='iPhone 6']").click({force: true})
-  //   cy.screenshot('tab')
+  it('should capture screenshot', ()=> {
+    cy.visit('https://demo-opencart.com/')
+    cy.wait(2000)
+    cy.screenshot('Homepage')
+    cy.wait(2000)
+    cy.get("img[title='Your Store']").screenshot('MyStore')
+    cy.wait(2000)
+    cy.get("img[alt='iPhone 6']").click({force: true})
+    cy.screenshot('tab')
 
-  // })
+  })
 
+  // We need to run this test on CLI so that cypress automatically captures Screenshots and Videos
+  // npx cypress run --spec cypress\e2e\ScreenshortsAndVideos.cy.js --config video=true
+  // Sometimes video does not gets captured so we are handling it on CLI by using "--config video=true"
   it('should automatically capture screenshots when failed', ()=> {
     cy.visit('https://demo-opencart.com/')
     cy.get("li:nth-child(7) a:nth-child(1)").click()
